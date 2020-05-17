a7450c0bb1f0e98dfe765ae5d36c0d5b90fdc63e
chenhongfei <chenhongfei@ruijie.com.cn>
2020-05-17 21:16:57 +0800

    需求文档

---
+++ raise/src/main/java/com/national/raise/web/Test/PoiTest.java
@@ -0,0 +1,37 @@
+package com.national.raise.web.Test;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileOutputStream;
+
+public class PoiTest {
+
+    public static void main(String[] args) throws Exception {
+
+        Workbook book = new HSSFWorkbook();
+
+        Sheet sheet = book.createSheet();
+
+        Row row = sheet.createRow(1);  //从0开始计数
+
+        Cell cell = row.createCell(1);
+
+        cell.setCellValue("poi写数据成功！！！");
+
+        Font font = book.createFont();
+
+        font.setFontName("微软雅黑");
+        font.setFontHeightInPoints((short) 20);
+
+        CellStyle style = book.createCellStyle();
+
+        style.setFont(font);
+
+        cell.setCellStyle(style);
+
+        book.write(new FileOutputStream("e:/poi.xls"));
+
+        book.close();
+    }
+}
