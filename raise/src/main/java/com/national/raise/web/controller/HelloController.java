fc40b63fbc51b2a538592e8774f2bd64ed8274dd
chenhongfei@ruijie.com.cn <chenhongfei@ruijie.com.cn>
2020-06-01 19:12:44 +0800

    前台上传页面

--- raise/src/main/java/com/national/raise/web/controller/HelloController.java
+++ raise/src/main/java/com/national/raise/web/controller/HelloController.java
@@ -6,7 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 public class HelloController {
@@ -24,5 +31,45 @@ public class HelloController {
     public MonthData queryById(@PathVariable Long id) {
 
         return monthDataService.queryById(id);
+    }
+
+    /**
+     * 本地访问内容地址 ：http://localhost:8080/lmycc/hello
+     */
+    @RequestMapping("/hello")
+    public String helloHtml(HashMap<String, Object> map) {
+        map.put("hello", "欢迎进入HTML页面");
+        return "/index";
+    }
+
+
+    @RequestMapping(value = "upload")
+    @ResponseBody
+    public String upload(@RequestParam("testUpload") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "文件为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        System.out.println("上传的文件名为：" + fileName);
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println("上传的后缀名为：" + suffixName);
+        // 文件上传后的路径
+        String filePath = "D://test//";
+        File dest = new File(filePath + fileName);
+        // 检测是否存在目录
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
     }
 }
